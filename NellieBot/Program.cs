7478ef310153db60ed481b98122da85d5fdca38a
Hopewelljnj <pkemailjj@gmail.com>
2024-06-10 09:41:04 -0400

    Update Program.cs
    Fixed old UserEvents references

--- NellieBot/Program.cs
+++ NellieBot/Program.cs
@@ -50,9 +50,9 @@ namespace NellieBot
         Timeout = TimeSpan.FromSeconds(30)
       });
 
-      discord.MessageCreated += UserEvents.MessageCreated;
-      discord.MessageUpdated += UserEvents.MessageUpdated;
-      // discord.MessageDeleted += UserEvents.MessageDeleted;
+      discord.MessageCreated += MessageEvents.MessageCreated;
+      discord.MessageUpdated += MessageEvents.MessageUpdated;
+      // discord.MessageDeleted += MessageEvents.MessageDeleted;
       discord.ModalSubmitted += ModalEvents.ModalSubmitted;
       discord.ThreadCreated += GuildEvents.ThreadCreated;
       // discord.GuildMemberAdded += MemberEvents.GuildMemberAdded;

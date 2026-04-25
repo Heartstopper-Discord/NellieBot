ba188b1f7b9f4a73c7d051acfe9153599c213217
Joseph Cryer <jcryer1234@gmail.com>
2026-04-25 13:44:07 +0100

    Uncomment event commands

--- NellieBot/Commands/UtilityCommands.cs
+++ NellieBot/Commands/UtilityCommands.cs
@@ -77,50 +77,50 @@ namespace NellieBot.Commands
     //   await ctx.EditResponseAsync("Working");
     // }
 
-    // [Command("eventquestion")]
-    // public async ValueTask EventQuestionCommand(SlashCommandContext ctx, [Description("The question to be asked")] string question,
-    //                                        [Description("The user who asked the question originally")] DiscordUser user)
-    // {
-    //   string message = $"## {question}\n*__Asked by__: {user.Mention}*\n## <:Leaf1:976072863855558706><:Leaf2:1064274309423579137><:Leaf3:1064274307951382721><:Leaf4:1064274305493516298>";
-    //   var msg = await ctx.Channel.SendMessageAsync(message);
-    //   await msg.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":arrow_up:"));
-    //   await new LogBuilder(LogType.Say)
-    //     .WithCodeField("Event message sent", message)
-    //     .WithAuthorFooter(ctx.Member)
-    //     .Send();
-    //   await ctx.RespondAsync("Message sent.", true);
-    // }
+    [Command("eventquestion")]
+    public async ValueTask EventQuestionCommand(SlashCommandContext ctx, [Description("The question to be asked")] string question,
+                                           [Description("The user who asked the question originally")] DiscordUser user)
+    {
+      string message = $"## {question}\n*__Asked by__: {user.Mention}*\n## <:Leaf1:976072863855558706><:Leaf2:1064274309423579137><:Leaf3:1064274307951382721><:Leaf4:1064274305493516298>";
+      var msg = await ctx.Channel.SendMessageAsync(message);
+      await msg.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":arrow_up:"));
+      await new LogBuilder(LogType.Say)
+        .WithCodeField("Event message sent", message)
+        .WithAuthorFooter(ctx.Member)
+        .Send();
+      await ctx.RespondAsync("Message sent.", true);
+    }
 
-    // [Command("eventpaq")]
-    // public async ValueTask EventPaqCommand(SlashCommandContext ctx)
-    // {
-    //   var modal = new DiscordInteractionResponseBuilder()
-    //     .WithTitle("Add Event Answer")
-    //     .WithCustomId("event-answer")
-    //     .AddComponents(
-    //       new DiscordTextInputComponent(
-    //         label: "Question",
-    //         customId: "question",
-    //         required: true,
-    //         style: DiscordTextInputStyle.Short
-    //     ))
-    //     .AddComponents(
-    //       new DiscordTextInputComponent(
-    //         label: "Answer",
-    //         customId: "answer",
-    //         required: true,
-    //         style: DiscordTextInputStyle.Paragraph,
-    //         placeholder: "No need to indent with >.",
-    //         max_length: 1800
-    //     ))
-    //     .AddComponents(
-    //       new DiscordTextInputComponent(
-    //         label: "Answer Source",
-    //         customId: "source",
-    //         required: true,
-    //         style: DiscordTextInputStyle.Short
-    //     ));
-    //   await ctx.RespondWithModalAsync(modal);
-    // }
+    [Command("eventpaq")]
+    public async ValueTask EventPaqCommand(SlashCommandContext ctx)
+    {
+      var modal = new DiscordInteractionResponseBuilder()
+        .WithTitle("Add Event Answer")
+        .WithCustomId("event-answer")
+        .AddComponents(
+          new DiscordTextInputComponent(
+            label: "Question",
+            customId: "question",
+            required: true,
+            style: DiscordTextInputStyle.Short
+        ))
+        .AddComponents(
+          new DiscordTextInputComponent(
+            label: "Answer",
+            customId: "answer",
+            required: true,
+            style: DiscordTextInputStyle.Paragraph,
+            placeholder: "No need to indent with >.",
+            max_length: 1800
+        ))
+        .AddComponents(
+          new DiscordTextInputComponent(
+            label: "Answer Source",
+            customId: "source",
+            required: true,
+            style: DiscordTextInputStyle.Short
+        ));
+      await ctx.RespondWithModalAsync(modal);
+    }
   }
 }
